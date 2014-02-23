e617562575a0ca3e73a7978a4df07dd3abda4618
oppih-374 <letwego28@gmail.com>
2014-02-23 20:38:05 +0800

    fix typo

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,15 @@
 class Contact < ActiveRecord::Base
   has_no_table
   
-  colum :name, :string
-  colum :email, :string
-  colun :content, :string
+  column :name, :string
+  column :email, :string
+  column :content, :string
   
   validates_presence_of :name
   validates_presence_of :email
   validates_presence_of :content
   validates_format_of :email,
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
-  validates_length_of :content, :maximun => 500
+  validates_length_of :content, :maximum => 500
     
 end
