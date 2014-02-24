41d7ff12015eb4bb0495d9921505a22d0f09d42d
oppih-374 <letwego28@gmail.com>
2014-02-24 23:07:31 +0800

    use the configration of sending-email from development to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,19 @@ LearnRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'learn-rails-001.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  # Send email in development mode
+  config.action_mailer.perform_delivery = true
 end
